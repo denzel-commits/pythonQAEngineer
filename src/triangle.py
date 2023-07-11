5a8955696103595661f4b90adeb4c986b2050415
denzel-commits <dguchinsky@yahoo.com>
2023-07-11 23:34:28 +0600

    Refactor: validate if params in ini are not str instance first

--- src/triangle.py
+++ src/triangle.py
@@ -4,10 +4,10 @@ from src.figure import Figure
 class Triangle(Figure):
 
     def __init__(self, side_a, side_b, side_c):
-        if side_a <= 0 or side_b <= 0 or side_c <= 0:
+        if isinstance(side_a, str) or isinstance(side_b, str) or isinstance(side_c, str):
             raise ValueError
 
-        if isinstance(side_a, str) or isinstance(side_b, str) or isinstance(side_c, str):
+        if side_a <= 0 or side_b <= 0 or side_c <= 0:
             raise ValueError
 
         self.name = "Triangle"
@@ -22,7 +22,11 @@ class Triangle(Figure):
     def get_area(self):
         s = (self._side_a + self._side_b + self._side_c) / 2
 
-        return (s * (s - self._side_a) * (s - self._side_b) * (s - self._side_c)) ** 0.5
+        return round(((s * (s - self._side_a) * (s - self._side_b) * (s - self._side_c)) ** 0.5), 2)
 
     def get_perimeter(self):
         return self._side_a + self._side_b + self._side_c
+
+    def __str__(self):
+        return f"{self.name}(side_a={self._side_a}, side_b={self._side_b}, side_c={self._side_c}, area={self.area}" \
+               f", perimeter={self.perimeter})"
